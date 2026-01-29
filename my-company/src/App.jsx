d2789190b55afb14bc470d21c52f1a36fb06e58b
Rose123345 <awukurose58@gmail.com>
2026-01-29 02:54:05 +0000

    add my company folder

---
+++ my-company/src/App.jsx
@@ -0,0 +1,25 @@
+
+import Home from "./components/Home";
+import About from "./components/About";
+import Services from "./components/Services";
+ import Contact from "./components/Contact";
+
+
+function App(){
+
+  return(
+
+    <> 
+    <Home />
+    <About/>
+    <Services/>
+    <Contact/>
+    
+    
+    </>
+    
+
+  );
+
+}
+export default App;
