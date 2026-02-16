2a3f9f28c74c054d387de7797d7b2b39de9f601a
Rose123345 <awukurose58@gmail.com>
2026-02-16 03:10:20 +0000

    Add recipe-sharing-platform project

---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="p-10">
+      <h1 className="text-3xl font-bold text-blue-500">
+        Recipe Sharing Platform
+      </h1>
+    </div>
+  );
+}
+
+export default App;
